0290ad01a8c1b367a346fd2d4777fb1ae3c0d723
Ubuntu <chenxie95@vm4docker-k80.mcygpeelexkuheynsztwd2qfwb.xx.internal.cloudapp.net>
2021-03-18 06:30:12 +0000

    add the script to mount docker under /mnt

---
+++ mountmntdir4dockervm.sh
@@ -0,0 +1,7 @@
+sudo docker rm -f $(docker ps -aq); docker rmi -f $(docker images -q)
+sudo systemctl stop docker
+sudo rm -rf /var/lib/docker
+sudo mkdir /var/lib/docker
+sudo mkdir /mnt/docker
+sudo mount --rbind /mnt/docker /var/lib/docker
+sudo systemctl start docker
