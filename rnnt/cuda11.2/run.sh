14189d48f7f2250ba9601cacd75b32b220384dc0
Ubuntu <chenxie95@cudabuild.ktwucef55fluxafkbd0nrwy10g.jx.internal.cloudapp.net>
2021-04-23 22:49:30 +0000

    remove cuda11.2

--- rnnt/cuda11.2/run.sh
+++
@@ -1,4 +0,0 @@
-docker build -t rnnt-cuda11.0 .
-docker tag rnnt-cuda11.0:latest chenxie95/rnnt-cuda11.0:torch1.8
-docker push chenxie95/rnnt-cuda11.0:torch1.8
-nvidia-docker run -it --name rnnt-cuda11.0 --mount src=/home/xieche/RNNTTrain.pytorch/speech_transducers/,target=/workspace/rnnt,type=bind --mount src=/raid/xieche/3khTrainData/,target=/data/3khTrainData,type=bind chenxie95/rnnt-cuda11.0:torch1.8
