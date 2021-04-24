07be5e51b84a479cfa2b6bbd1bfe08ca91a957e4
Ubuntu <chenxie95@cudabuild.ktwucef55fluxafkbd0nrwy10g.jx.internal.cloudapp.net>
2021-04-24 06:00:11 +0000

    minor update

--- rnnt/cuda11.1/run.sh
+++ rnnt/cuda11.1/run.sh
@@ -1,4 +1,8 @@
 docker build -t rnnt-cuda11.1 .
-docker tag rnnt-cuda11.1:latest chenxie95/rnnt-cuda11.1:latest
-docker push chenxie95/rnnt-cuda11.1:latest
-nvidia-docker run -it --name rnnt-cuda11.1 --mount src=/home/xieche/RNNTTrain.pytorch/speech_transducers/,target=/workspace/rnnt,type=bind --mount src=/raid/xieche/3khTrainData/,target=/data/3khTrainData,type=bind chenxie95/rnnt-cuda11.1:latest
+# manually install apex
+sudo nvidia-docker run -it rnnt-cuda11.1:latest
+sudo docker commit <container-id>  rnnt-cuda11.1:torch1.8
+
+sudo docker tag rnnt-cuda11.1:torch1.8 chenxie95/rnnt-cuda11.1:latest
+sudo docker push chenxie95/rnnt-cuda11.1:latest
+sudo nvidia-docker run -it --name rnnt-cuda11.1 --mount src=/home/xieche/RNNTTrain.pytorch/speech_transducers/,target=/workspace/rnnt,type=bind --mount src=/raid/xieche/3khTrainData/,target=/data/3khTrainData,type=bind chenxie95/rnnt-cuda11.1:latest
